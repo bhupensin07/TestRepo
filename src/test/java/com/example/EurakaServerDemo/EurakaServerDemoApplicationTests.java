580722a70f931656f5f943804d34041eaae480e4
Bhupendra Singh <bhupendra.singh@symmedia.de>
2023-12-20 15:45:45 +0530

    Initial commit

---
+++ src/test/java/com/example/EurakaServerDemo/EurakaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurakaServerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
